d62daac61b827aa1f2d38e57d137317b83416073
YangGwangmo <kuyang95@naver.com>
2021-11-01 18:50:11 +0900

    상태바, 툴바 제작

--- app/src/main/java/com/pang/docfriendstest/MainActivity.kt
+++ app/src/main/java/com/pang/docfriendstest/MainActivity.kt
@@ -2,10 +2,34 @@ package com.pang.docfriendstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
+import com.pang.docfriendstest.databinding.ActivityMainBinding
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // 왼쪽 버튼, 기본값 뒤로가기 버튼
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_search_w) // 왼쪽 버튼 아이콘 변경
+
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom, menu)
+        return true
     }
 }
